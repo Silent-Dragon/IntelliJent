73fbe6c3d268f1468ae453e34381b425fa5aa17e
Gabi Campos <corp@livware.com>
2018-12-29 17:41:49 +0000

    Initial commit for IntelliJent

---
+++ day4/D0401_p2_JS/js/script.js
@@ -0,0 +1,24 @@
+// background-image: url("../imgs/03_Pictures/Gallery_00.jpg");
+
+//alert('This is from an external JS script');
+
+function showPic1() {
+    //alert(1);
+    document.getElementById("frameDiv2")
+        .style.backgroundImage = 'url("imgs/03_Pictures/Gallery_01.jpg")';
+}
+
+function showPic2() {
+    document.getElementById("frameDiv2")
+        .style.backgroundImage = 'url("imgs/03_Pictures/Gallery_02.jpg")';
+}
+
+function showPic3() {
+    document.getElementById("frameDiv2")
+        .style.backgroundImage = 'url("imgs/03_Pictures/Gallery_03.jpg")';
+}
+
+function showPic4() {
+    document.getElementById("frameDiv2")
+        .style.backgroundImage = 'url("imgs/03_Pictures/Gallery_04.jpg")';
+}
